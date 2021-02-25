51039d6c361a21578c4a684afb604adf4df7c259
dhcalderon <dhcalder@ucsd.edu>
2021-02-25 10:25:37 -0800

    finished it

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = "IUfVZsAe4QScwjwf9KO7Mv8GSqBUlW6A";
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+	  center: [32.8785248,-117.23808],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 15
+	});
 }
